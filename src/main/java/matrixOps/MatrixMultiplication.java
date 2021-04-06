f4f7f4812714b7e239a47c3b32db4d49abd00c2b
Girija Arani <aranigirja@gmail.com>
2021-04-06 12:21:02 +0530

    matrix multiplication

--- src/main/java/matrixOps/MatrixMultiplication.java
+++ src/main/java/matrixOps/MatrixMultiplication.java
@@ -4,26 +4,41 @@ import java.util.Scanner;
 
 public class MatrixMultiplication {
 
-    public void multiplyMatrices() {
-        acceptMatrixAsInputFromStandardInput(); // what do I do with this? how many times do I call this?
-        performMultiplication();
+    public static void main(String[] args) {
+        multiplyMatrices();
+    }
+
+    public static void multiplyMatrices() {
+        int[][] matrix1 = new int[10][10];
+        int[][] matrix2 = new int[10][10];
+        matrix1 = acceptMatrixAsInputFromStandardInput();
+        matrix2 = acceptMatrixAsInputFromStandardInput();
+        // what do I do with this? how many times do I call this?
+        performMultiplication(matrix1, matrix2);
+
     }
 
     /**
      * A method to read a matrix of specific dimensions
      * from standard input
+     *
      * @return a multidimensional array containing the matrix
      */
-    public int[][] acceptMatrixAsInputFromStandardInput() {
+    public static int[][] acceptMatrixAsInputFromStandardInput() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter number of rows: ");
         int nRows = scanner.nextInt();
+
         System.out.println("Enter number of columns: ");
+        int nCols = scanner.nextInt();
         // what happens here?
 
         int[][] matrix = new int[10][10];
         for (int row = 0; row < nRows; row++) {
+            for (int col = 0; col < nCols; col++) {
+                matrix[row][col] = scanner.nextInt();
+            }
             // and here?
         }
         return matrix;
@@ -32,7 +47,25 @@ public class MatrixMultiplication {
     /**
      * A method to perform the actual matrix multiplication
      */
-    public void performMultiplication() {
+    public static void performMultiplication(int[][] matrix1, int[][] matrix2) {
         // should this function accept parameters?
+        for (int i = 0; i < 3; i++) {
+            int[][] product = new int[10][10];
+
+            for (int j = 0; j < 3; j++) {
+                product[i][j] = 0;
+                for (int k = 0; k < 3; k++) {
+
+                    product[i][j] = product[i][j] + matrix1[i][k] * matrix2[k][j];
+                }
+                for (i = 0; i < 3; i++) {
+                    for (j = 0; j < 3; j++) {
+                        System.out.print(product[i][j]+"");
+                    }
+                    System.out.println("");
+                }
+            }
+
+        }
     }
 }
